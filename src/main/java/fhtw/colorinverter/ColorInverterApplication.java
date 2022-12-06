a3a1a5842fd10099cf15ebbfdc2386598fb66257
Fabian Steiner <fabian.franz.steiner@aon.at>
2022-12-06 19:14:01 +0100

    #1 added Maven dependencies

--- src/main/java/fhtw/colorinverter/ColorInverterApplication.java
+++ src/main/java/fhtw/colorinverter/ColorInverterApplication.java
@@ -2,8 +2,6 @@ package fhtw.colorinverter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class ColorInverterApplication {
