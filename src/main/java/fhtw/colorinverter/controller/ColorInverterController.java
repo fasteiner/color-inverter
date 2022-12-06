0729f70008affe8489be61b46dc7183ff9824587
Fabian Steiner <fabian.franz.steiner@aon.at>
2022-12-06 19:05:29 +0100

    #1 added Maven dependencies

--- src/main/java/fhtw/colorinverter/controller/ColorInverterController.java
+++ src/main/java/fhtw/colorinverter/controller/ColorInverterController.java
@@ -4,6 +4,7 @@ import fhtw.colorinverter.service.ColorInverterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
