d094cb63d5b3b88ef441824e286873913a373962
Fabian Steiner <fabian.franz.steiner@aon.at>
2022-12-06 18:03:37 +0100

    init

---
+++ src/main/java/fhtw/colorinverter/ColorInverterApplication.java
@@ -0,0 +1,13 @@
+package fhtw.colorinverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorInverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorInverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fhtw/colorinverter/ColorInverterApplicationTests.java
@@ -0,0 +1,13 @@
+package fhtw.colorinverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorInverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
